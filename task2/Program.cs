798cb2a44fbb34da9a1b6d3790c2c15820d346e9
Ivan <IvanBWork@yandex.ru>
2023-11-22 19:51:20 +0300

    finish code task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int Input(string text)
+{
+  Console.Write(text);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AkkermanFunction(m - 1, 1);
+    }
+    else
+    {
+        return (AkkermanFunction(m - 1, AkkermanFunction(m, n - 1)));
+    }
+}
+
+int m = Input("Введите число M: ");
+int n = Input("Введите число N: ");
+
+Console.Write($"Результат: {AkkermanFunction(m, n)}");
