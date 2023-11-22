9d4fede233e275325fb69d544e22c7c96a4a9c81
Ivan <IvanBWork@yandex.ru>
2023-11-22 19:32:08 +0300

    finish code task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int Input(string text)
+{
+  Console.Write(text);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+void PrintNumbers(int m, int n)
+{
+  if (n > m) PrintNumbers(m, n - 1);
+  Console.Write($"{n} ");
+}
+
+int m = Input("Введите первое число: ");
+int n = Input("Введите второе число: ");
+
+Console.WriteLine("Результат: ");
+PrintNumbers(m, n);
